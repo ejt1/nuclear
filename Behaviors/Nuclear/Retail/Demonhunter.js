61a8848af70af0ee2a5bbf850cb2b579e2002f93
SovietFarmer <Soviet@wector.com>
2024-08-11 09:34:29 +1000

    Start of Havoc demonhunter

---
+++ Behaviors/Nuclear/Retail/Demonhunter.js
@@ -0,0 +1,55 @@
+import { Behavior, BehaviorContext } from "../../../Core/Behavior";
+import * as bt from '../../../Core/BehaviorTree';
+import Specialization from '../../../Core/Specialization';
+import common from '../../../Core/Common';
+import spell from "../../../Core/Spell";
+import {me} from "../../../Core/ObjectManager";
+
+export class DemonhunterHavocBehavior extends Behavior {
+  context = BehaviorContext.Any; // PVP ?
+  specialization = Specialization.DemonHunter.Havoc;
+  version = wow.GameVersion.Retail;
+
+  build() {
+    return new bt.Decorator(
+      ret => !spell.isGlobalCooldown(),
+      new bt.Selector(
+        common.waitForTarget(),
+        common.waitForCastOrChannel(),
+        spell.cast("Throw Glaive", null, null, ret => me.power > 45), // should be a range check such as NOT target.HasAura("Master of the Glaive")
+        spell.cast("Essence Break", null, null, ret => true), // should check cds 
+        spell.cast("Eye Beam", null, null, ret => me.power > 49),
+        this.isMetamorphosis()
+          ? spell.cast("Death Sweep", null, null, ret => me.power > 50)
+          : spell.cast("Blade Dance", null, null, ret => me.power > 50),
+        this.isMetamorphosis()
+          ? spell.cast("Annihilation", null, null, ret => me.power > 50)
+          : spell.cast("Chaos Strike", null, null, ret => me.power > 50),
+        spell.cast("Felblade", null, ret => true, null),
+        spell.cast("Throw Glaive", null, null, ret => me.power > 80),
+      )
+    );
+  }
+
+  hasAura(auras, name) {
+    // should change to hasAuras by Me, and check hasCaster & caster guid === me?
+    return auras.some(aura => aura.name === name);
+  }
+
+  getAura(auras, name) {
+    for (let aura of auras) {
+      if (aura.name === name) {
+        return aura;
+      }
+    }
+    return null; // Return null if the aura is not found
+  }
+
+  isMetamorphosis() {
+    if (this.hasAura(me.auras, "Metamorphosis")) {
+      console.debug("For it is ---- metamorphosis!");
+      return true;
+    }
+    return false;
+  }
+}
