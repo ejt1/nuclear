4ccc3c18fb07bb08971be6ccd430c77540974d1e
ejt1 <github@wector.info>
2024-08-13 11:31:07 +0200

    fix: check game ready before ticking behaviors

--- nuclear.js
+++ nuclear.js
@@ -2,6 +2,7 @@ import { BehaviorContext } from "./Core/Behavior";
 import BehaviorBuilder from "./Core/BehaviorBuilder";
 import objMgr from "./Core/ObjectManager";
 import { me } from './Core/ObjectManager';
+import { flagsComponents } from "./Core/Util";
 
 class Nuclear extends wow.EventListener {
   async initialize() {
@@ -11,14 +12,16 @@ class Nuclear extends wow.EventListener {
   }
 
   tick() {
-    if (me) {
-      try {
-        this.rootBehavior?.tick();
-      } catch (e) {
-        this.rootBehavior = null;
-        console.error(`${e.message}`);
-        console.error(`${e.stack}`);
-      }
+    if (!this.gameReady()) {
+      return;
+    }
+
+    try {
+      this.rootBehavior?.tick();
+    } catch (e) {
+      this.rootBehavior = null;
+      console.error(`${e.message}`);
+      console.error(`${e.stack}`);
     }
   }
 
@@ -34,6 +37,20 @@ class Nuclear extends wow.EventListener {
     if (event.name == 'PLAYER_ENTERING_WORLD') {
       this.rebuild();
     }
+  }
+
+  gameReady() {
+    const gameui = new wow.CGGameUI;
+    if (gameui.state != this.previous_state) {
+      console.debug(`state changed to ${flagsComponents(gameui.state, 16)}`);
+      this.previous_state = gameui.state;
+    }
+    // XXX: figure out game state flags, 0x211 is "in game" mask for retail
+    if (gameui.state != 0x211) {
+      return false;
+    }
+
+    return me ? true : false;
   }
 }
 
