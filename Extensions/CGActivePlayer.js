88aabaa05870f7fbb77cacdc69f1f2323fc169a9
SovietFarmer <Soviet@wector.com>
2024-08-21 17:23:41 +1000

    Comments.

--- Extensions/CGActivePlayer.js
+++ Extensions/CGActivePlayer.js
@@ -1,15 +1,14 @@
-import objMgr, {me} from "../Core/ObjectManager";
 
 
 Object.defineProperties(wow.CGActivePlayer.prototype, {
 
   currentParty: {
     /**
-     * Get the current active party if it exists, or return `undefined` if not in a party.
+     * Get the current active party if it exists.
      * @returns {wow.Party | undefined} - Returns the current party instance or undefined.
      */
     get: function () {
-      return (wow.Party?.currentParty) ? wow.Party.currentParty : undefined;
+      return wow.Party.currentParty;
     }
   }
 
