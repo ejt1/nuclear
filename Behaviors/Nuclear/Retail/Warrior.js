d747c9b12b770eb686f5597e1aab6fe72739f5ee
ejt1 <github@wector.info>
2024-08-29 17:58:39 +0200

    warrior: update rotation

--- Behaviors/Nuclear/Retail/Warrior.js
+++ Behaviors/Nuclear/Retail/Warrior.js
@@ -12,20 +12,44 @@ export class WarriorFuryBehavior extends Behavior {
 
   build() {
     return new bt.Decorator(
-      ret => !spell.isGlobalCooldown(),
+      ret => !spell.isGlobalCooldown() && !me.isMounted,
       new bt.Selector(
         common.waitForNotMounted(),
         common.waitForTarget(),
         common.waitForCastOrChannel(),
+        common.waitForFacing(),
 
         spell.cast("Battle Shout", on => me, req => !me.hasAuraByMe("Battle Shout")),
-        spell.cast("Execute"),
+
+        spell.cast("Victory Rush", ret => me.pctHealth < 70),
+        spell.cast("Rampage", ret => !this.isEnraged()),
+        new bt.Decorator(
+          this.isEnraged,
+          new bt.Selector(
+            spell.cast("Thunder Clap", ret => me.hasAuraByMe("Thunder Blast")),
+            spell.cast("Avatar"),
+            spell.cast("Recklessness"),
+            spell.cast("Blood Fury"),
+            spell.cast("Thunderous Roar"),
+            spell.cast("Odyn's Fury"),
+            spell.cast("Bladestorm"),
+          )
+        ),
         spell.cast("Rampage"),
-        spell.cast("Raging Blow"),
-        //spell.cast("Bloodbath"),
+        spell.cast("Crushing Blow"),
+        spell.cast("Bloodbath"),
+        spell.cast("Raging Blow", ret => this.isEnraged() || me.power < 110),
+        spell.cast("Rampage"),
+        spell.cast("Execute", ret => me.hasAuraByMe("Sudden Death")),
         spell.cast("Bloodthirst"),
+        spell.cast("Thunder Clap"),
         spell.cast("Whirlwind"),
       )
     );
+  }
+
+  isEnraged(){
+    const enrage = me.getAuraByMe("Enrage");
+    return enrage === undefined || enrage.remaining < 600;
   }
 }
