6e92d8dde8e06117ef0ceb6c8d39d8d3bfc78ae2
Neer <legitting@gmail.com>
2024-09-15 14:38:14 +0200

    restructure Pet helper

--- Core/Pet.js
+++ Core/Pet.js
@@ -2,14 +2,31 @@ import { me } from "@/Core/ObjectManager";
 import * as bt from '@/Core/BehaviorTree';
 
 class Pet {
+  // Static properties
+  static getStance = () => wow.PetInfo.stance;
+  static getCommand = () => wow.PetInfo.command;
+  static isAttackActive = () => wow.PetInfo.attackActive;
+  static getPetGuid = () => wow.PetInfo.guid;
+  static getAllPets = () => wow.PetInfo.pets;
+  static getActions = () => wow.PetInfo.actions;
+
+  static isAlive() {
+    return this.current && this.current.health > 0;
+  }
+
+  static get current() {
+    return wow.PetInfo.pets[0]?.toUnit() ?? null;
+  }
+
+  // Behavior tree actions
   static attack(targetSelector) {
     return new bt.Action(() => {
       const target = targetSelector();
-      if (!me.pet || !target) return bt.Status.Failure;
+      const pet = this.current;
+      if (!pet || !target) return bt.Status.Failure;
 
-      if (!me.pet.target?.equals(target.guid)) {
+      if (!pet.target?.equals(target.guid)) {
         wow.PetInfo.sendAction(wow.PetInfo.actions[0], target.guid);
-        return bt.Status.Failure;
       }
 
       return bt.Status.Failure;
@@ -18,22 +35,16 @@ class Pet {
 
   static follow(conditionSelector) {
     return new bt.Action(() => {
-      if (!me.pet?.target || !conditionSelector() || !wow.PetInfo.attackActive) {
+      const pet = this.current;
+      if (!pet?.target || !conditionSelector() || !wow.PetInfo.attackActive) {
         return bt.Status.Failure;
       }
 
       wow.PetInfo.sendAction(wow.PetInfo.actions[1], me.guid);
 
-      return me.pet.targetGuid?.equals(me.guid) ? bt.Status.Failure : bt.Status.Failure;
+      return pet.targetGuid?.equals(me.guid) ? bt.Status.Failure : bt.Status.Failure;
     });
   }
-
-  static getStance = () => wow.PetInfo.stance;
-  static getCommand = () => wow.PetInfo.command;
-  static isAttackActive = () => wow.PetInfo.attackActive;
-  static getPetGuid = () => wow.PetInfo.guid;
-  static getAllPets = () => wow.PetInfo.pets;
-  static getActions = () => wow.PetInfo.actions;
 }
 
 export default Pet;
