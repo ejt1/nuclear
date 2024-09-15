3a78379b6b7405f3c1ea287bb08c7447d47ca6e7
Neer <legitting@gmail.com>
2024-09-15 11:33:13 +0200

    Fix option slicing

--- Extra/General.js
+++ Extra/General.js
@@ -32,9 +32,9 @@ class General {
     renderFunction([
       { header: "Combat Behavior", options: this.options.slice(0, 5) },
       { header: "Spell Casting", options: this.options.slice(5, 7) },
-      { header: "Interrupt", options: this.options.slice(7, 10) },
-      { header: "Dispel", options: this.options.slice(10, 12) },
-      { header: "Healthstone", options: this.options.slice(12) },
+      { header: "Interrupt", options: this.options.slice(7, 9) },
+      { header: "Dispel", options: this.options.slice(9, 11) },
+      { header: "Healthstone", options: this.options.slice(11) },
     ]);
   }
 
