431fc4986b6de1aeec32ccb690f720d60b48576f
SovietFarmer <Soviet@wector.com>
2024-10-07 14:46:17 +1100

    Holy Pally PVP, added use smacky hands on FDK.

--- Behaviors/Nuclear/Retail/Paladin/PaladinHolyPvP.js
+++ Behaviors/Nuclear/Retail/Paladin/PaladinHolyPvP.js
@@ -9,17 +9,11 @@ import {DispelPriority} from "@/Data/Dispels"
 import {WoWDispelType} from "@/Enums/Auras";
 import spellBlacklist from "@/Data/PVPData";
 import { defaultCombatTargeting as combat } from "@/Targeting/CombatTargeting";
+import { PowerType } from "@/Enums/PowerType";
 
 const auras = {
   infusionOfLight: 54149,
-  powerOfTheDarkSide: 198068,
-  purgeTheWicked: 204213,
-  powerWordShield: 17,
-  atonement: 194384,
-  surgeOfLight: 114255,
-  premonitionPiety: 428930,
-  premonitionSolace: 428934,
-  premonitionInsight: 428933,
+  forbearance: 25771,
 };
 
 export class PaladinHolyPvP extends Behavior {
@@ -36,6 +30,7 @@ export class PaladinHolyPvP extends Behavior {
         common.waitForNotSitting(),
         common.waitForNotMounted(),
         common.waitForCastOrChannel(),
+        spell.cast("Divine Protection", on => me, ret => me.pctHealth < 40),
         this.healRotation(),
         common.waitForTarget(),
         common.waitForFacing(),
@@ -54,16 +49,22 @@ export class PaladinHolyPvP extends Behavior {
       // // If talented, maintain Beacon of Faith on second target
       // spell.cast("Beacon of Faith", on => h.getPriorityPVPHealTarget(), ret => spell.isSpellKnown("Beacon of Faith") && this.findSecondaryHealTarget() !== undefined),
       spell.interrupt("Rebuke", true),
+      // lay on hands last resort
+      spell.cast("Lay on Hands", on => h.getPriorityPVPHealTarget(), ret => this.doesNotHaveForbearance(h.getPriorityPVPHealTarget()) && h.getPriorityPVPHealTarget().pctHealth < 20),
+      // Use Word of Glory when reaching 5 Holy Power
+      spell.cast("Word of Glory", on => h.getPriorityPVPHealTarget(), ret => me.powerByType(PowerType.HolyPower) > 4 && h.getPriorityPVPHealTarget()?.pctHealth < 90),
+      // Use sacrifice
+      spell.cast("Blessing of Sacrifice", on => h.getPriorityPVPHealTarget(), ret => me.pctHealth > 80 && h.getPriorityPVPHealTarget().pctHealth < 39),
       // Cast Divine Toll for Holy Power burst generation
-      spell.cast("Divine Toll", on => h.getPriorityPVPHealTarget(), ret => h.getPriorityPVPHealTarget()?.pctHealth < 35),
+      spell.cast("Divine Toll", on => h.getPriorityPVPHealTarget(), ret => h.getPriorityPVPHealTarget()?.pctHealth < 45),
       // Cast Barrier of Faith (if talented) for shields and Divine Favor
       spell.cast("Barrier of Faith", on => h.getPriorityPVPHealTarget(), ret => h.getPriorityPVPHealTarget()?.pctHealth < 50 && spell.isSpellKnown("Barrier of Faith")),
       // Use Word of Glory when reaching 3 Holy Power
-      spell.cast("Word of Glory", on => h.getPriorityPVPHealTarget(), ret => h.getPriorityPVPHealTarget()?.pctHealth < 65),
+      spell.cast("Word of Glory", on => h.getPriorityPVPHealTarget(), ret => h.getPriorityPVPHealTarget()?.pctHealth < 75 &&  me.powerByType(PowerType.HolyPower) > 2),
       // Use Flash of Light for fast emergency healing
-      spell.cast("Flash of Light", on => h.getPriorityPVPHealTarget(), ret => h.getPriorityPVPHealTarget()?.pctHealth < 50),
+      spell.cast("Flash of Light", on => h.getPriorityPVPHealTarget(), ret => h.getPriorityPVPHealTarget()?.pctHealth < 60),
       // Use Infusion of Light procs on Holy Light for bigger heals
-      spell.cast("Holy Light", on => h.getPriorityPVPHealTarget(), ret => me.hasAura(auras.infusionOfLight) && h.getPriorityPVPHealTarget()?.pctHealth < 75),
+      spell.cast("Holy Light", on => h.getPriorityPVPHealTarget(), ret => me.hasAura(auras.infusionOfLight) && h.getPriorityPVPHealTarget()?.pctHealth < 75 && me.getPlayerEnemies(20).length === 0),
       // Dispel harmful debuffs (Purify)
       spell.dispel("Cleanse", true, DispelPriority.High, true, WoWDispelType.Magic, WoWDispelType.Poison, WoWDispelType.Disease),
       spell.cast("Blessing of Freedom", on => heal.friends.All.find(unit => unit.isRooted() || unit.isSlowed())),
@@ -71,6 +72,16 @@ export class PaladinHolyPvP extends Behavior {
       spell.cast("Holy Shock", on => h.getPriorityPVPHealTarget(), ret => h.getPriorityPVPHealTarget()?.pctHealth < 95),
 
   );
+  }
+
+  doesNotHaveForbearance(target) {
+    if (!target) {
+      return false;
+    }
+    if (target.hasAura(auras.forbearance)) {
+      return false;
+    }
+    return true;
   }
 
   // Damage Rotation
@@ -86,7 +97,6 @@ export class PaladinHolyPvP extends Behavior {
       spell.cast("Judgment", on => me.targetUnit),
       // Crusader Strike for Holy Power generation
       spell.cast("Crusader Strike", on => me.targetUnit),
-
     );
   }
 
