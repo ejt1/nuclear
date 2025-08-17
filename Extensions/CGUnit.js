aeb79e44b47ffae0b4b0bb5bb33f3daaa311acc4
Neer <legitting@gmail.com>
2025-08-17 08:40:22 +0200

    Refactor PaladinProtection behavior to modularize rotations for interrupts, taunts, defenses, emergency healing, and damage. Enhance target selection methods for various spells and improve overall logic for better performance.

--- Extensions/CGUnit.js
+++ Extensions/CGUnit.js
@@ -558,6 +558,10 @@ Object.defineProperties(wow.CGUnit.prototype, {
         return true
       }
 
+      if (me.isWithinMeleeRange(target)) {
+        return true;
+      }
+
       target = target instanceof wow.CGUnit ? target : target.toUnit();
       if (!target || !target.position || !this.position) {
         return false;
